82e22b285dd30393fa04a7d7892f32986cb6f70d
Santiago Alvarez <saalvarezva@gmail.com>
2017-01-20 01:38:06 -0500

    Demo Source

---
+++ Assets/Scripts/BumperController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperController : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        anim.Play("Hit");
+    }    
+
+}
---
+++ Assets/Scripts/FlapperController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlapperController : MonoBehaviour
+{
+    public KeyCode keyToControl;
+
+	void Update () {
+        //When a key is pressed, turn on the motor
+        if (Input.GetKey(keyToControl))
+        {
+            GetComponent<HingeJoint2D>().useMotor = true;
+        }
+        else
+        {
+            GetComponent<HingeJoint2D>().useMotor = false;
+        }
+	}
+}
